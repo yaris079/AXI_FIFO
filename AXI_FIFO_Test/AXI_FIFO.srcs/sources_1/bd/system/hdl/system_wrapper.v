c304891e5fb8439ed818c4e89fdd9bc51dabbdf2
unknown <yaris079@outlook.com>
2018-08-08 15:34:20 +0800

    first commit

---
+++ AXI_FIFO_Test/AXI_FIFO.srcs/sources_1/bd/system/hdl/system_wrapper.v
@@ -0,0 +1,160 @@
+//Copyright 1986-2015 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2015.4 (win64) Build 1412921 Wed Nov 18 09:43:45 MST 2015
+//Date        : Tue Jul 31 15:40:48 2018
+//Host        : DESKTOP-HDMLCJ0 running 64-bit major release  (build 9200)
+//Command     : generate_target system_wrapper.bd
+//Design      : system_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module system_wrapper
+   (DDR_addr,
+    DDR_ba,
+    DDR_cas_n,
+    DDR_ck_n,
+    DDR_ck_p,
+    DDR_cke,
+    DDR_cs_n,
+    DDR_dm,
+    DDR_dq,
+    DDR_dqs_n,
+    DDR_dqs_p,
+    DDR_odt,
+    DDR_ras_n,
+    DDR_reset_n,
+    DDR_we_n,
+    FCLK_CLK1,
+    FIXED_IO_ddr_vrn,
+    FIXED_IO_ddr_vrp,
+    FIXED_IO_mio,
+    FIXED_IO_ps_clk,
+    FIXED_IO_ps_porb,
+    FIXED_IO_ps_srstb,
+    M_AXIS_tdata,
+    M_AXIS_tkeep,
+    M_AXIS_tlast,
+    M_AXIS_tready,
+    M_AXIS_tvalid,
+    S_AXIS_tdata,
+    S_AXIS_tkeep,
+    S_AXIS_tlast,
+    S_AXIS_tready,
+    S_AXIS_tvalid,
+    debug_tri_i,
+    gpio_rtl_tri_o,
+    led_tri_o,
+    peripheral_aresetn);
+  inout [14:0]DDR_addr;
+  inout [2:0]DDR_ba;
+  inout DDR_cas_n;
+  inout DDR_ck_n;
+  inout DDR_ck_p;
+  inout DDR_cke;
+  inout DDR_cs_n;
+  inout [3:0]DDR_dm;
+  inout [31:0]DDR_dq;
+  inout [3:0]DDR_dqs_n;
+  inout [3:0]DDR_dqs_p;
+  inout DDR_odt;
+  inout DDR_ras_n;
+  inout DDR_reset_n;
+  inout DDR_we_n;
+  output FCLK_CLK1;
+  inout FIXED_IO_ddr_vrn;
+  inout FIXED_IO_ddr_vrp;
+  inout [53:0]FIXED_IO_mio;
+  inout FIXED_IO_ps_clk;
+  inout FIXED_IO_ps_porb;
+  inout FIXED_IO_ps_srstb;
+  output [31:0]M_AXIS_tdata;
+  output [3:0]M_AXIS_tkeep;
+  output M_AXIS_tlast;
+  input M_AXIS_tready;
+  output M_AXIS_tvalid;
+  input [31:0]S_AXIS_tdata;
+  input [3:0]S_AXIS_tkeep;
+  input S_AXIS_tlast;
+  output S_AXIS_tready;
+  input S_AXIS_tvalid;
+  input [1:0]debug_tri_i;
+  output [0:0]gpio_rtl_tri_o;
+  output [1:0]led_tri_o;
+  output [0:0]peripheral_aresetn;
+
+  wire [14:0]DDR_addr;
+  wire [2:0]DDR_ba;
+  wire DDR_cas_n;
+  wire DDR_ck_n;
+  wire DDR_ck_p;
+  wire DDR_cke;
+  wire DDR_cs_n;
+  wire [3:0]DDR_dm;
+  wire [31:0]DDR_dq;
+  wire [3:0]DDR_dqs_n;
+  wire [3:0]DDR_dqs_p;
+  wire DDR_odt;
+  wire DDR_ras_n;
+  wire DDR_reset_n;
+  wire DDR_we_n;
+  wire FCLK_CLK1;
+  wire FIXED_IO_ddr_vrn;
+  wire FIXED_IO_ddr_vrp;
+  wire [53:0]FIXED_IO_mio;
+  wire FIXED_IO_ps_clk;
+  wire FIXED_IO_ps_porb;
+  wire FIXED_IO_ps_srstb;
+  wire [31:0]M_AXIS_tdata;
+  wire [3:0]M_AXIS_tkeep;
+  wire M_AXIS_tlast;
+  wire M_AXIS_tready;
+  wire M_AXIS_tvalid;
+  wire [31:0]S_AXIS_tdata;
+  wire [3:0]S_AXIS_tkeep;
+  wire S_AXIS_tlast;
+  wire S_AXIS_tready;
+  wire S_AXIS_tvalid;
+  wire [1:0]debug_tri_i;
+  wire [0:0]gpio_rtl_tri_o;
+  wire [1:0]led_tri_o;
+  wire [0:0]peripheral_aresetn;
+
+  system system_i
+       (.DDR_addr(DDR_addr),
+        .DDR_ba(DDR_ba),
+        .DDR_cas_n(DDR_cas_n),
+        .DDR_ck_n(DDR_ck_n),
+        .DDR_ck_p(DDR_ck_p),
+        .DDR_cke(DDR_cke),
+        .DDR_cs_n(DDR_cs_n),
+        .DDR_dm(DDR_dm),
+        .DDR_dq(DDR_dq),
+        .DDR_dqs_n(DDR_dqs_n),
+        .DDR_dqs_p(DDR_dqs_p),
+        .DDR_odt(DDR_odt),
+        .DDR_ras_n(DDR_ras_n),
+        .DDR_reset_n(DDR_reset_n),
+        .DDR_we_n(DDR_we_n),
+        .FCLK_CLK1(FCLK_CLK1),
+        .FIXED_IO_ddr_vrn(FIXED_IO_ddr_vrn),
+        .FIXED_IO_ddr_vrp(FIXED_IO_ddr_vrp),
+        .FIXED_IO_mio(FIXED_IO_mio),
+        .FIXED_IO_ps_clk(FIXED_IO_ps_clk),
+        .FIXED_IO_ps_porb(FIXED_IO_ps_porb),
+        .FIXED_IO_ps_srstb(FIXED_IO_ps_srstb),
+        .M_AXIS_tdata(M_AXIS_tdata),
+        .M_AXIS_tkeep(M_AXIS_tkeep),
+        .M_AXIS_tlast(M_AXIS_tlast),
+        .M_AXIS_tready(M_AXIS_tready),
+        .M_AXIS_tvalid(M_AXIS_tvalid),
+        .S_AXIS_tdata(S_AXIS_tdata),
+        .S_AXIS_tkeep(S_AXIS_tkeep),
+        .S_AXIS_tlast(S_AXIS_tlast),
+        .S_AXIS_tready(S_AXIS_tready),
+        .S_AXIS_tvalid(S_AXIS_tvalid),
+        .debug_tri_i(debug_tri_i),
+        .gpio_rtl_tri_o(gpio_rtl_tri_o),
+        .led_tri_o(led_tri_o),
+        .peripheral_aresetn(peripheral_aresetn));
+endmodule
