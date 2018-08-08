3e726f436bf29a778cf295f70bec8ae7c91f9ae7
unknown <yaris079@outlook.com>
2018-08-08 15:20:11 +0800

    first commit

---
+++ AXI_FIFO_RW/AXI_FIFO.sdk/FIFO_Test/Debug/src/dma_intr.d
@@ -0,0 +1,69 @@
+src/dma_intr.o src/dma_intr.d: ../src/dma_intr.c ../src/dma_intr.h \
+ ../../FIFO_Test_bsp/ps7_cortexa9_0/include/xaxidma.h \
+ ../../FIFO_Test_bsp/ps7_cortexa9_0/include/xaxidma_bdring.h \
+ ../../FIFO_Test_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../FIFO_Test_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../FIFO_Test_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../FIFO_Test_bsp/ps7_cortexa9_0/include/xaxidma_bd.h \
+ ../../FIFO_Test_bsp/ps7_cortexa9_0/include/xaxidma_hw.h \
+ ../../FIFO_Test_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../FIFO_Test_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../FIFO_Test_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../FIFO_Test_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../FIFO_Test_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../FIFO_Test_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../FIFO_Test_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../FIFO_Test_bsp/ps7_cortexa9_0/include/xdebug.h \
+ ../../FIFO_Test_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../FIFO_Test_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../FIFO_Test_bsp/ps7_cortexa9_0/include/xil_exception.h \
+ ../../FIFO_Test_bsp/ps7_cortexa9_0/include/xdebug.h \
+ ../../FIFO_Test_bsp/ps7_cortexa9_0/include/xscugic.h \
+ ../../FIFO_Test_bsp/ps7_cortexa9_0/include/xscugic_hw.h \
+ ../../FIFO_Test_bsp/ps7_cortexa9_0/include/xil_exception.h
+
+../src/dma_intr.h:
+
+../../FIFO_Test_bsp/ps7_cortexa9_0/include/xaxidma.h:
+
+../../FIFO_Test_bsp/ps7_cortexa9_0/include/xaxidma_bdring.h:
+
+../../FIFO_Test_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../FIFO_Test_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../FIFO_Test_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../FIFO_Test_bsp/ps7_cortexa9_0/include/xaxidma_bd.h:
+
+../../FIFO_Test_bsp/ps7_cortexa9_0/include/xaxidma_hw.h:
+
+../../FIFO_Test_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../FIFO_Test_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../FIFO_Test_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../FIFO_Test_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../FIFO_Test_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../FIFO_Test_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../FIFO_Test_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../FIFO_Test_bsp/ps7_cortexa9_0/include/xdebug.h:
+
+../../FIFO_Test_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../FIFO_Test_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../FIFO_Test_bsp/ps7_cortexa9_0/include/xil_exception.h:
+
+../../FIFO_Test_bsp/ps7_cortexa9_0/include/xdebug.h:
+
+../../FIFO_Test_bsp/ps7_cortexa9_0/include/xscugic.h:
+
+../../FIFO_Test_bsp/ps7_cortexa9_0/include/xscugic_hw.h:
+
+../../FIFO_Test_bsp/ps7_cortexa9_0/include/xil_exception.h:
---
+++ AXI_FIFO_RW/AXI_FIFO.sdk/FIFO_Test/Debug/src/main.d
@@ -0,0 +1,100 @@
+src/main.o src/main.d: ../src/main.c ../src/dma_intr.h \
+ ../../FIFO_Test_bsp/ps7_cortexa9_0/include/xaxidma.h \
+ ../../FIFO_Test_bsp/ps7_cortexa9_0/include/xaxidma_bdring.h \
+ ../../FIFO_Test_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../FIFO_Test_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../FIFO_Test_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../FIFO_Test_bsp/ps7_cortexa9_0/include/xaxidma_bd.h \
+ ../../FIFO_Test_bsp/ps7_cortexa9_0/include/xaxidma_hw.h \
+ ../../FIFO_Test_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../FIFO_Test_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../FIFO_Test_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../FIFO_Test_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../FIFO_Test_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../FIFO_Test_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../FIFO_Test_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../FIFO_Test_bsp/ps7_cortexa9_0/include/xdebug.h \
+ ../../FIFO_Test_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../FIFO_Test_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../FIFO_Test_bsp/ps7_cortexa9_0/include/xil_exception.h \
+ ../../FIFO_Test_bsp/ps7_cortexa9_0/include/xdebug.h \
+ ../../FIFO_Test_bsp/ps7_cortexa9_0/include/xscugic.h \
+ ../../FIFO_Test_bsp/ps7_cortexa9_0/include/xscugic_hw.h \
+ ../../FIFO_Test_bsp/ps7_cortexa9_0/include/xil_exception.h \
+ ../src/timer_intr.h ../../FIFO_Test_bsp/ps7_cortexa9_0/include/xadcps.h \
+ ../../FIFO_Test_bsp/ps7_cortexa9_0/include/xadcps_hw.h \
+ ../../FIFO_Test_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../FIFO_Test_bsp/ps7_cortexa9_0/include/Xscugic.h \
+ ../../FIFO_Test_bsp/ps7_cortexa9_0/include/Xil_exception.h \
+ ../../FIFO_Test_bsp/ps7_cortexa9_0/include/xscutimer.h \
+ ../../FIFO_Test_bsp/ps7_cortexa9_0/include/xscutimer_hw.h \
+ ../src/sys_intr.h ../../FIFO_Test_bsp/ps7_cortexa9_0/include/xgpio.h \
+ ../../FIFO_Test_bsp/ps7_cortexa9_0/include/xgpio_l.h
+
+../src/dma_intr.h:
+
+../../FIFO_Test_bsp/ps7_cortexa9_0/include/xaxidma.h:
+
+../../FIFO_Test_bsp/ps7_cortexa9_0/include/xaxidma_bdring.h:
+
+../../FIFO_Test_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../FIFO_Test_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../FIFO_Test_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../FIFO_Test_bsp/ps7_cortexa9_0/include/xaxidma_bd.h:
+
+../../FIFO_Test_bsp/ps7_cortexa9_0/include/xaxidma_hw.h:
+
+../../FIFO_Test_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../FIFO_Test_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../FIFO_Test_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../FIFO_Test_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../FIFO_Test_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../FIFO_Test_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../FIFO_Test_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../FIFO_Test_bsp/ps7_cortexa9_0/include/xdebug.h:
+
+../../FIFO_Test_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../FIFO_Test_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../FIFO_Test_bsp/ps7_cortexa9_0/include/xil_exception.h:
+
+../../FIFO_Test_bsp/ps7_cortexa9_0/include/xdebug.h:
+
+../../FIFO_Test_bsp/ps7_cortexa9_0/include/xscugic.h:
+
+../../FIFO_Test_bsp/ps7_cortexa9_0/include/xscugic_hw.h:
+
+../../FIFO_Test_bsp/ps7_cortexa9_0/include/xil_exception.h:
+
+../src/timer_intr.h:
+
+../../FIFO_Test_bsp/ps7_cortexa9_0/include/xadcps.h:
+
+../../FIFO_Test_bsp/ps7_cortexa9_0/include/xadcps_hw.h:
+
+../../FIFO_Test_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../FIFO_Test_bsp/ps7_cortexa9_0/include/Xscugic.h:
+
+../../FIFO_Test_bsp/ps7_cortexa9_0/include/Xil_exception.h:
+
+../../FIFO_Test_bsp/ps7_cortexa9_0/include/xscutimer.h:
+
+../../FIFO_Test_bsp/ps7_cortexa9_0/include/xscutimer_hw.h:
+
+../src/sys_intr.h:
+
+../../FIFO_Test_bsp/ps7_cortexa9_0/include/xgpio.h:
+
+../../FIFO_Test_bsp/ps7_cortexa9_0/include/xgpio_l.h:
